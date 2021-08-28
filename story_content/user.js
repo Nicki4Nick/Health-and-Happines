054df2da21fe602a8d97740df39124a4d27d19b7
Nicki4Nick <53259017+Nicki4Nick@users.noreply.github.com>
2021-08-28 15:19:32 +0400

    fsgfdg

---
+++ story_content/user.js
@@ -0,0 +1,303 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6gE3aFuA3mk":
+        Script1();
+        break;
+      case "6eRAGru66rO":
+        Script2();
+        break;
+      case "5WpH8ZrLn6e":
+        Script3();
+        break;
+      case "6BjnQUJf8gS":
+        Script4();
+        break;
+      case "6QdrLNfrcvR":
+        Script5();
+        break;
+      case "6ZGp5PNdpQx":
+        Script6();
+        break;
+      case "6pavMT1QT6x":
+        Script7();
+        break;
+      case "5Z6Dl1dyjmn":
+        Script8();
+        break;
+      case "5cQxQv9qKnD":
+        Script9();
+        break;
+      case "6oggaiQLuHw":
+        Script10();
+        break;
+      case "5iY0ywhN7Vg":
+        Script11();
+        break;
+      case "5hDbEFmFjcz":
+        Script12();
+        break;
+      case "5sAU1GKwfMg":
+        Script13();
+        break;
+      case "6VaZZ761B5A":
+        Script14();
+        break;
+      case "6CfePnkq8Ks":
+        Script15();
+        break;
+      case "6nFPqe2Iugo":
+        Script16();
+        break;
+      case "6CtaAMkbyvY":
+        Script17();
+        break;
+      case "5bhoAcVPSTE":
+        Script18();
+        break;
+      case "5s0VWthe8RQ":
+        Script19();
+        break;
+      case "5nZM4AzlAZQ":
+        Script20();
+        break;
+      case "63W5S3VeuSO":
+        Script21();
+        break;
+      case "6gZbfDtR3J8":
+        Script22();
+        break;
+      case "5d1n4yQM0As":
+        Script23();
+        break;
+      case "5pYkjU1Gowp":
+        Script24();
+        break;
+      case "69KbRRTkbVv":
+        Script25();
+        break;
+      case "6LclwCz5TnU":
+        Script26();
+        break;
+      case "5WvkET6ClEX":
+        Script27();
+        break;
+      case "5fQROCecQrY":
+        Script28();
+        break;
+      case "5jGO98crFas":
+        Script29();
+        break;
+      case "5ngEmN0PNv6":
+        Script30();
+        break;
+      case "5uguJVHN6Ab":
+        Script31();
+        break;
+      case "5XOicO69VmL":
+        Script32();
+        break;
+      case "6LxA1dLFj9r":
+        Script33();
+        break;
+      case "6klgvAFOehH":
+        Script34();
+        break;
+      case "6Izb2oJUm5w":
+        Script35();
+        break;
+      case "5bcPdD6pOu0":
+        Script36();
+        break;
+      case "6g6bTWnr9cs":
+        Script37();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzgxfDE3ODB8YWQ5NDk4Zjk2NTI5OTE3YThkYTY1NDNkZmIxOGVjODk";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script2()
+{
+  FullscreenMode('slide');
+}
+
+function Script3()
+{
+  FullscreenMode('slide');
+}
+
+function Script4()
+{
+  FullscreenMode('slide');
+}
+
+function Script5()
+{
+  FullscreenMode('slide');
+}
+
+function Script6()
+{
+  FullscreenMode('slide');
+}
+
+function Script7()
+{
+  FullscreenMode('slide');
+}
+
+function Script8()
+{
+  FullscreenMode('slide');
+}
+
+function Script9()
+{
+  FullscreenMode('slide');
+}
+
+function Script10()
+{
+  FullscreenMode('slide');
+}
+
+function Script11()
+{
+  FullscreenMode('slide');
+}
+
+function Script12()
+{
+  FullscreenMode('slide');
+}
+
+function Script13()
+{
+  FullscreenMode('slide');
+}
+
+function Script14()
+{
+  FullscreenMode('slide');
+}
+
+function Script15()
+{
+  FullscreenMode('slide');
+}
+
+function Script16()
+{
+  FullscreenMode('slide');
+}
+
+function Script17()
+{
+  FullscreenMode('slide');
+}
+
+function Script18()
+{
+  FullscreenMode('slide');
+}
+
+function Script19()
+{
+  FullscreenMode('slide');
+}
+
+function Script20()
+{
+  FullscreenMode('slide');
+}
+
+function Script21()
+{
+  FullscreenMode('slide');
+}
+
+function Script22()
+{
+  window.print();
+}
+
+function Script23()
+{
+  FullscreenMode('slide');
+}
+
+function Script24()
+{
+  FullscreenMode('slide');
+}
+
+function Script25()
+{
+  FullscreenMode('slide');
+}
+
+function Script26()
+{
+  FullscreenMode('slide');
+}
+
+function Script27()
+{
+  FullscreenMode('slide');
+}
+
+function Script28()
+{
+  window.print();
+}
+
+function Script29()
+{
+  window.print();
+}
+
+function Script30()
+{
+  FullscreenMode('slide');
+}
+
+function Script31()
+{
+  FullscreenMode('slide');
+}
+
+function Script32()
+{
+  FullscreenMode('slide');
+}
+
+function Script33()
+{
+  FullscreenMode('slide');
+}
+
+function Script34()
+{
+  FullscreenMode('slide');
+}
+
+function Script35()
+{
+  window.print();
+}
+
+function Script36()
+{
+  window.print();
+}
+
+function Script37()
+{
+  window.print();
+}
+
